4a1e386fdc8d2ff9fc24898151b37d8240929e25
bobosun0713 <chiabo.sun@gmail.com>
2020-05-11 13:34:31 +0800

    輪播修改

--- dest/js/all.js
+++ dest/js/all.js
@@ -1,9 +1,13 @@
 window.addEventListener(
     "load",
     function () {
-        let num = 0
-        var Left_btn = document.querySelector(".Left_btn")
-        var Right_btn = document.querySelector(".Righit_btn")
+        var Left_btn = document.querySelectorAll(".Left_btn")
+        var Right_btn = document.querySelectorAll(".Righit_btn")
+        var Right_btn_length = document.querySelectorAll(".Righit_btn").length
+        var Left_btn_length = document.querySelectorAll(".Left_btn").length
+        var Read_cardSpac_div = document.querySelectorAll(".read_cardSpac")
+
+        // 點點控制鈕變數
         var read_cardSpac = document.querySelector(".read_cardSpac")
         var read_cardSpac_div = read_cardSpac.getElementsByClassName("space").length
         var p_space = document.querySelector(".p_space")
@@ -15,41 +19,42 @@ window.addEventListener(
             li.setAttribute("class", "clickMe")
             p_space.appendChild(li)
         }
-        // for (let i = 1; i <= read_cardSpac_div; i++) {
-        var clickMe = document.querySelector(".clickMe")
-        // }
-        clickMe.onclick = function () {
-            alert("1")
-        }
-        // 左按鈕
-        Left_btn.onclick = function () {
-            let width = 1550
-            num++
-            read_cardSpac.style.left = -width * num + "px"
-            Right_btn.disabled = false
-            if (num >= 4) {
-                Left_btn.disabled = true
+
+        // 註冊事件處理
+        for (let i = 0; i < read_cardSpac_div; i++) {
+            var clickMe = document.querySelectorAll(".clickMe")
+            clickMe[i].onclick = function () {
+                alert(i)
             }
         }
-        // 右按鈕
-        Right_btn.onclick = function () {
-            let width = 1550
-            num--
 
-            read_cardSpac.style.left = -width * num + "px"
-            Left_btn.disabled = false
-            if (num == 0) {
-                Right_btn.disabled = true
+        // 左按鈕
+        for (let i = 0; i < Left_btn_length; i++) {
+            let num = 0
+            Left_btn[i].onclick = function (e) {
+                let width = 1550
+                num++
+                console.log(num)
+
+                Read_cardSpac_div[i].style.left = -width * num + "px"
+                Right_btn[i].disabled = false
+                if (num >= read_cardSpac_div - 1) {
+                    Left_btn[i].disabled = true
+                }
+            }
+
+            Right_btn[i].onclick = function () {
+                let width = 1550
+                num--
+                console.log(num)
+
+                Read_cardSpac_div[i].style.left = -width * num + "px"
+                Left_btn[i].disabled = false
+                if (num == 0) {
+                    Right_btn[i].disabled = true
+                }
             }
         }
     },
     false
 )
-
-function a() {
-    function c() {
-        console.log(b)
-    }
-    var b = 3
-    c()
-}
