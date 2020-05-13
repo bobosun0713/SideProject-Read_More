6b7e3567986345c15869c8177222d116789d578d
bobosun0713 <chiabo.sun@gmail.com>
2020-05-13 22:33:14 +0800

    第二次上傳

--- dest/js/all.js
+++ dest/js/all.js
@@ -10,14 +10,19 @@ window.addEventListener(
         // 點點控制鈕變數
         var read_cardSpac = document.querySelector(".read_cardSpac")
         var read_cardSpac_div = read_cardSpac.getElementsByClassName("space").length
-        var p_space = document.querySelector(".p_space")
+        var p_space = document.querySelectorAll(".p_space")
+        var p_space_length = document.querySelectorAll(".p_space").length
 
         // 點點控治鈕
         for (let i = 0; i < read_cardSpac_div; i++) {
             // 動態新增li標籤 （用來做點點控制鈕）
-            var li = document.createElement("li")
-            li.setAttribute("class", "clickMe")
-            p_space.appendChild(li)
+            var li_1 = document.createElement("li")
+            li_1.setAttribute("class", "clickMe")
+            var li_2 = document.createElement("li")
+            li_2.setAttribute("class", "clickMe1")
+
+            p_space[0].appendChild(li_1)
+            p_space[1].appendChild(li_2)
         }
 
         // 註冊事件處理
@@ -31,27 +36,43 @@ window.addEventListener(
         // 左按鈕
         for (let i = 0; i < Left_btn_length; i++) {
             let num = 0
-            Left_btn[i].onclick = function (e) {
+            let a = 0
+            clickMe[a].style.background = "red"
+            Left_btn[i].onclick = function () {
                 let width = 1550
+                a++
                 num++
-                console.log(num)
+                if (num <= read_cardSpac_div - 1) {
+                    Read_cardSpac_div[i].style.left = -width * num + "px"
+                    Right_btn[i].disabled = false
 
-                Read_cardSpac_div[i].style.left = -width * num + "px"
-                Right_btn[i].disabled = false
-                if (num >= read_cardSpac_div - 1) {
-                    Left_btn[i].disabled = true
+                    // 點點顏色提示
+                    // console.log(a)
+                    if (a <= read_cardSpac_div - 1) {
+                        clickMe[a].style.background = "red"
+                        clickMe[a - 1].style.background = ""
+                    }
+                } else if (num > read_cardSpac_div - 1) {
+                    Read_cardSpac_div[i].style.left = 0 + "px"
+                    // 當num大於卡片內容數 最後點點顏色消失
+                    clickMe[4].style.background = ""
+                    // 這時回到第一張 手動讓點點顏色回到最初顏色
+                    clickMe[0].style.background = "red"
+                    num = 0
+                    a = 0
                 }
             }
 
             Right_btn[i].onclick = function () {
                 let width = 1550
                 num--
-                console.log(num)
-
                 Read_cardSpac_div[i].style.left = -width * num + "px"
                 Left_btn[i].disabled = false
-                if (num == 0) {
-                    Right_btn[i].disabled = true
+                if (num < 0) {
+                    num = 4
+                    Read_cardSpac_div[i].style.left = -width * num + "px"
+                } else if (num > 0) {
+                    Read_cardSpac_div[i].style.left = -width * num + "px"
                 }
             }
         }
